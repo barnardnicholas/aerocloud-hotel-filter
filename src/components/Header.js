8ce7cc24d4f631a29d316b5cb56dd19156d299ca
Nick <barnard.nicholas@googlemail.com>
2020-02-11 15:04:15 +0000

    add contact details to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ const Header = () => {
   return (
     <header>
       <h1>AeroCloud Hotel Finder</h1>
-      <h2>Nick Barnard - Tech Test</h2>
+      <h2>Nick Barnard - February 2020</h2>
+      <p>barnard.nicholas@googlemail.com</p>
     </header>
   );
 };
