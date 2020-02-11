06272c2674a5bd52fe7c58f364b07b9faa1e8d90
Nick <barnard.nicholas@googlemail.com>
2020-02-11 15:20:14 +0000

    detectMatch passes test 1

--- src/spec/utils.spec.js
+++ src/spec/utils.spec.js
@@ -2,10 +2,10 @@ const { expect } = require("chai");
 const { detectMatch } = require("../utils/utils");
 
 describe("detectMatch", () => {
-  it("returns an array", () => {
+  it("returns a boolean", () => {
     const hotel = { name: "", starRating: 0, facilities: [] };
-    const searchTerm = [];
-    const actualResult = utils.detectMatch(hotel, searchTerm);
-    expect(actualResult).to.be.an("object");
+    const searchTerms = [];
+    const actualResult = detectMatch(hotel, searchTerms);
+    expect(actualResult).to.be.a("boolean");
   });
 });
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,8 @@
-const detectMatch = hotel => {};
+// Takes a hotel object and an array of search terms and returns true/false if all facilities are present
+
+const detectMatch = (hotel, searchTerms) => {
+  return true;
+};
 
 module.exports = {
   detectMatch
