65c6916758e27e4a7e23269063c8b9b639d26908
Nick <barnard.nicholas@googlemail.com>
2020-02-11 15:26:29 +0000

    detectMactch passes test 3

--- src/spec/utils.spec.js
+++ src/spec/utils.spec.js
@@ -8,10 +8,16 @@ describe("detectMatch", () => {
     const actualResult = detectMatch(hotel, searchTerms);
     expect(actualResult).to.be.a("boolean");
   });
-  it("2 - Returns true for a single search term", () => {
+  it("2 - Returns true for a single correct search term", () => {
     const hotel = { name: "", starRating: 0, facilities: ["car park"] };
     const searchTerms = ["car park"];
     const actualResult = detectMatch(hotel, searchTerms);
     expect(actualResult).to.equal(true);
+  });
+  it("3 - Returns false for a single incorrect search term", () => {
+    const hotel = { name: "", starRating: 0, facilities: ["pool"] };
+    const searchTerms = ["car park"];
+    const actualResult = detectMatch(hotel, searchTerms);
+    expect(actualResult).to.equal(false);
   });
 });
