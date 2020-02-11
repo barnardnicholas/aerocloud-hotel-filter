fdf16555ec0d21e6636fff9042b47c08f9e820da
Nick <barnard.nicholas@googlemail.com>
2020-02-11 15:25:08 +0000

    detectMatch passed test 2

--- src/spec/utils.spec.js
+++ src/spec/utils.spec.js
@@ -2,10 +2,16 @@ const { expect } = require("chai");
 const { detectMatch } = require("../utils/utils");
 
 describe("detectMatch", () => {
-  it("returns a boolean", () => {
+  it("1 - Returns a boolean", () => {
     const hotel = { name: "", starRating: 0, facilities: [] };
     const searchTerms = [];
     const actualResult = detectMatch(hotel, searchTerms);
     expect(actualResult).to.be.a("boolean");
+  });
+  it("2 - Returns true for a single search term", () => {
+    const hotel = { name: "", starRating: 0, facilities: ["car park"] };
+    const searchTerms = ["car park"];
+    const actualResult = detectMatch(hotel, searchTerms);
+    expect(actualResult).to.equal(true);
   });
 });
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,14 @@
 // Takes a hotel object and an array of search terms and returns true/false if all facilities are present
 
 const detectMatch = (hotel, searchTerms) => {
-  return true;
+  const { facilities } = hotel;
+  let result = true;
+  searchTerms.forEach(term => {
+    if (!facilities.includes(term)) {
+      result = false;
+    }
+  });
+  return result;
 };
 
 module.exports = {
